cbe5db6aefeea15bf6ad4c8d3053135589d8904b
Robson Alves <106483036+robsonalves04@users.noreply.github.com>
2025-07-10 17:24:58 +0100

    Setup inicial do projeto e implementação de dependencias no gradle e telas de inicio

---
+++ app/src/main/java/com/example/cat_app/navigate/AppNavigation.kt
@@ -0,0 +1,9 @@
+package com.example.cat_app.navigate
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun AppNavigation() {}
---
+++ app/src/main/java/com/example/cat_app/screen/ScreenMain.kt
@@ -0,0 +1,20 @@
+package com.example.cat_app.screen
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import com.example.cat_app.navigate.AppNavigation
+
+class ScreenMain : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppNavigation()
+        }
+    }
+}
